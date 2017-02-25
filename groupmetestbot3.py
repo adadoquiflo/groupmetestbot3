fb3cf0809bfcdb2c6e854b049a4057fe93ff4ca2
adadoquiflo <adadoquiflo@users.noreply.github.com>
2017-02-25 16:22:52 -0500

    Update groupmetestbot3.py

--- groupmetestbot3.py
+++ groupmetestbot3.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+import SocketServer
+from BaseHTTPServer import BaseHTTPRequestHandler
+
+def some_function():
+    print "some_function got called"
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        some_function()
+
+        self.send_response(200)
+
+httpd = SocketServer.TCPServer(("", 8080), MyHandler)
+httpd.serve_forever()
